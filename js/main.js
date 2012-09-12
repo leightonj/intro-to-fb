0e76636844f0e67f2170155ebc6aa2978c135581
Leighton James <leighton@theiplatform.com>
2012-09-12 14:50:47 +0100

    Work

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function() { 
     $('#a_share').click(function() {
+        feed();
+    });
+    
+    $('#a_sendRequestToRecipients').click(function() {
+        sendRequestToRecipients();
+    });
+    
+    $('#a_sendRequestViaMultiFriendSelector').click(function() {
+        sendRequestViaMultiFriendSelector();
+    });
+    
+    function feed() {
         var obj = {
             method: 'feed',
             link: 'http://intro-to-fb.theiplatform.info/4_index.html',
@@ -16,5 +28,28 @@ $(document).ready(function() {
         }
         
         FB.ui(obj, callback);
-    });
+    }
+    
+    function sendRequestToRecipients() {
+        FB.ui({
+            method: 'apprequests',
+            message: 'My Great Request',
+            to: '3616968,1370233776', 
+            link: 'http://intro-to-fb.theiplatform.info/4_index.html',
+            picture: 'http://intro-to-fb.theiplatform.info/img/facebook_icon_200_2.jpg'
+        }, requestCallback);
+    }
+
+    function sendRequestViaMultiFriendSelector() {
+        FB.ui({
+            method: 'apprequests',
+            message: 'My Great Request', 
+            link: 'http://intro-to-fb.theiplatform.info/4_index.html',
+            picture: 'http://intro-to-fb.theiplatform.info/img/facebook_icon_200_2.jpg'
+        }, requestCallback);
+    }
+
+    function requestCallback(response) {
+    // Handle callback here
+    }
 });
